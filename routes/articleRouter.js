db4815cb510ed3650bde9998135ba43c5b1bdcb7
Jaswanth <119675027+Jaswant10041@users.noreply.github.com>
2025-09-05 16:48:02 +0530

    for_showing_postsdefaultly

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -4,7 +4,7 @@ const verifyJWT = require('../middleware/verifyJWT');
 const articleRouter=require('express').Router();
 
 articleRouter.post('/add',verifyJWT,articleController.addArticle);
-articleRouter.get('/posts',verifyJWT,articleController.getPosts);
+articleRouter.get('/posts',articleController.getPosts);
 articleRouter.post('/deletepost',verifyJWT,articleController.deletePost);
 
 module.exports=articleRouter;
